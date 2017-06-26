e4dc6e8d5ea976bcd7a7a75e989b52ff711b3b84
kwitophurr <arcjohns@ucsc.edu>
2017-06-25 21:00:54 -0700

    Brought over new plotting functions from old code

--- election_analysis.py
+++ election_analysis.py
@@ -13,6 +13,190 @@ import cartopy.io.shapereader as shpreader
 import load_data as ld
 
 
+def state_extent(name):
+    """
+    elif name == 'RI':
+    elif name == 'SC':
+    elif name == 'SD':
+    elif name == 'TN':
+    elif name == 'TX':
+    elif name == 'UT':
+    elif name == 'VT':
+    elif name == 'VA':
+    elif name == 'WA':
+    elif name == 'WV':
+    elif name == 'WI':
+    elif name == 'WY':
+    """   
+    """
+    elif name == 'ND':
+    elif name == 'OH':
+    elif name == 'OK':
+    elif name == 'OR':
+    """
+    """
+    elif name == 'KY':
+    elif name == 'LA':
+    elif name == 'ME':
+    elif name == 'MD':
+    elif name == 'MA':
+    elif name == 'MI':
+    elif name == 'MN':
+    elif name == 'MS':
+    elif name == 'MO':
+    elif name == 'MT':
+    elif name == 'NE':
+    elif name == 'NV':
+    elif name == 'NH':
+    elif name == 'NJ':
+    elif name == 'NM':
+    elif name == 'NY':
+    """
+    
+    #Change to be around state of interest
+    if name =='AL':
+        return [-89.0, -84.5, 30.0, 35.4]
+    elif name == 'AZ':
+        return [-115.7, -108.5, 30.0, 35.7]
+    elif name == 'AR':
+        return [-95.43, -89.37, 32.78, 35.2]
+    elif name == 'CA':
+        return [-125.3, -113.7, 32.0, 40.0]
+    elif name == 'CO':
+        return [-110.0, -101.3, 36.14, 41.36]
+    elif name == 'CT':
+        return [-74.12, -71.47, 38.8, 39.9]
+    elif name == 'DE':
+        return [-76.0, -75.0, 36.6, 38.0]
+    elif name == 'FL':
+        return [-88.0,-79., 23., 31.]
+    elif name == 'GA':
+        return [-86.1, -80.44, 30.06, 34.0]
+    elif name == 'HI':
+        return [-162.0, -152., 18.,23.]
+    elif name == 'ID':
+        return [-118.0, -110.5, 39.2, 45.7]
+    elif name == 'IL':
+        return [-92.24, -86.91, 36.74, 40.4]
+    elif name == 'IN':
+        return [-88.43, -84.53, 35.9, 41.95]
+    elif name == 'IA':
+        return [-96.98, -89.8, 40.14, 43.87]
+    elif name == 'KS':
+        return [-102.8, -94.2, 35.04, 38.3]
+    elif name == 'NC':
+        return [-85.0,-74., 32.,36.]
+    elif name == 'PA':
+        return [-81.0, -74., 37., 40.]
+    else:
+        return [-150, -70, 20, 45]
+    
+def state_plot_data_model(fips_indices, test_outputs, test_results, name):
+    print name
+    fig = plt.figure(figsize=(12.0, 5.0))
+    ax1 = plt.axes([0.0, 0.0, 0.45, 1.0],projection=ccrs.Miller(), aspect=1.3)
+    ax2 = plt.axes([0.45, 0.0, 0.45, 1.0],projection=ccrs.Miller(), aspect=1.3)
+    ax1.set_extent(state_extent(name))
+    ax2.set_extent(state_extent(name))
+
+    data = test_results
+    filename = 'cb_2015_us_county_5m/cb_2015_us_county_5m.shp'
+    for state, record in zip(shpreader.Reader(filename).geometries(), shpreader.Reader(filename).records()):
+        id = str(int(record.__dict__['attributes']['GEOID']))
+        if id in dict(zip(map(str, map(int, fips_indices)),data)):
+            min_val = -0.45
+            max_val = 0.45
+            data = test_outputs-0.5
+            value = (dict(zip(map(str, map(int,fips_indices)),data))[id]-min_val)/(max_val-min_val)
+            ax1.add_geometries(state, crs=ccrs.Miller(), facecolor=cm.bwr(value), edgecolor='black', linewidth=0.2)
+            
+            min_val = -0.45
+            max_val = 0.45
+            data = test_results-0.5
+            value = (dict(zip(map(str, map(int,fips_indices)),data))[id]-min_val)/(max_val-min_val)
+            ax2.add_geometries(state, crs=ccrs.Miller(), facecolor=cm.bwr(value), edgecolor='black', linewidth=0.2)
+
+    print "Plotting..."
+    ax1.background_patch.set_visible(False)
+    ax1.outline_patch.set_visible(False)
+    ax2.background_patch.set_visible(False)
+    ax2.outline_patch.set_visible(False)
+    ax1.set_title('Data')
+    ax2.set_title('Model')
+    #Add colorbar
+    axc = plt.axes([0.9, 0.25, 0.02, 0.5], frameon=False)
+    cmap = cm.seismic
+    norm = mpl.colors.Normalize(vmin=-45.0, vmax=+45.0)
+    cb = mpl.colorbar.ColorbarBase(axc, cmap=cmap,norm=norm,orientation='vertical')
+    cb.set_label('Vote Margin [%]')
+    
+    #fig.suptitle('2016 US Presidential Election Results Residual: '+name)
+    plt.savefig('plots/election_data_model_'+name+'.pdf',bbox_inches='tight')
+    plt.show()
+
+
+def state_plot_residual(fips_indices, test_outputs, test_results, name, X, Y, training_results, all_fips_indices):
+    print name
+    fig = plt.figure(figsize=(12.0, 7.0))
+    ax1 = plt.axes([0.0, 0.0, 0.45, 1.0],projection=ccrs.Miller(), aspect=1.3)
+    ax2 = plt.axes([0.45, 0.0, 0.45, 1.0],projection=ccrs.Miller(), aspect=1.3)
+    ax1.set_extent(state_extent(name))
+    ax2.set_extent(state_extent(name))
+    
+    data = test_results
+    filename = 'cb_2015_us_county_5m/cb_2015_us_county_5m.shp'
+    for state, record in zip(shpreader.Reader(filename).geometries(), shpreader.Reader(filename).records()):
+        id = str(int(record.__dict__['attributes']['GEOID']))
+        if id in dict(zip(map(str, map(int, fips_indices)),data)):
+
+            min_val = -0.2
+            max_val = 0.2
+            data = test_outputs-test_results
+            value = (dict(zip(map(str, map(int,fips_indices)),data))[id]-min_val)/(max_val-min_val)
+            ax1.add_geometries(state, crs=ccrs.Miller(), facecolor=cm.bwr(value), edgecolor='black', linewidth=0.2)
+            
+            min_val = -5.0
+            max_val = 5.0
+            data = sigma(test_outputs-test_results, fips_indices, X, Y, training_results, all_fips_indices)
+            value = (dict(zip(map(str, map(int,fips_indices)),data))[id]-min_val)/(max_val-min_val)
+            ax2.add_geometries(state, crs=ccrs.Miller(), facecolor=cm.bwr(value), edgecolor='black', linewidth=0.2)
+            
+    print "Plotting..."
+    ax1.background_patch.set_visible(False)
+    ax1.outline_patch.set_visible(False)
+    ax2.background_patch.set_visible(False)
+    ax2.outline_patch.set_visible(False)
+    
+    #Add colorbar
+    axc = plt.axes([0.85, 0.25, 0.02, 0.5], frameon=False)
+    cmap = cm.seismic
+    norm = mpl.colors.Normalize(vmin=min_val, vmax=max_val)
+    cb = mpl.colorbar.ColorbarBase(axc, cmap=cmap,norm=norm,orientation='vertical')
+    cb.set_label('GOP Vote Fraction [%]')
+    
+    #fig.suptitle('2016 US Presidential Election Results Residual: '+name)
+    plt.savefig('plots/election_residuals_'+name+'.pdf',bbox_inches='tight')
+    plt.show()
+
+#For sklearn, we need the training data organized as a numpy array, with rows = counties and columns = data points
+#Ideally we will normalize everything to be within [-1, +1], and set missing values to be 0
+#Strategy: loop through each data file (i.e. loop through columns)
+
+#Return array of significances for each county, given the spread of uncertainty in similar counties
+def sigma(resid, test_fips, X, Y, training_results, fips_indices):
+    sigmas = np.zeros((len(resid)))
+    j = 0
+    for county_fips in test_fips:
+        chi2_vals = np.zeros((3140))
+        #Calculate chi2 between county inputs and the rest of the country
+        county_row = np.argmin(np.abs(fips_indices-county_fips))
+        chi2_vals = np.sum((X-X[county_row,:])**2, axis=1)
+        training_indices = np.argsort(chi2_vals[np.nonzero(chi2_vals)])[:15]
+        sigmas[j] = resid[j]/np.std(Y[training_indices]-training_results[training_indices])
+        j += 1
+    return sigmas
+
+
 #Define plotting function, so we can see the results in a nice way
 def national_plot(fips_indices, data):
     power = 0
@@ -254,17 +438,19 @@ else:
 
 
 
-states={'AL':'01','AK':'02','AZ':'04','AR':'05','CA':'06','CO':'08','CT':'09','DE':'10','DC':'11','FL':'12','GA':'13','HI':'15','ID':'16','IL':'17','IN':'18','IA':'19','KS':'20','KY':'21','LA':'22','ME':'23','MD':'24','MA':'25','MI':'26','MN':'27','MS':'28','MO':'29','MT':'30','NE':'31','NV':'32','NH':'33','NJ':'34','NM':'35','NY':'36','NC':'37','ND':'38','OH':'39','OK':'40','OR':'41','PA':'42','RI':'44','SC':'45','SD':'46','TN':'47','TX':'48','UT':'49','VT':'50','VA':'51','WA':'53','WV':'54','WI':'55','WY':'56'}
-test_state = 'NC'
-def run_neural_net(test_state, states=states):
+
+states_codes={'AL':'01','AZ':'04','AR':'05','CA':'06','CO':'08','CT':'09','DE':'10','FL':'12','GA':'13','HI':'15','ID':'16','IL':'17','IN':'18','IA':'19','KS':'20','KY':'21','LA':'22','ME':'23','MD':'24','MA':'25','MI':'26','MN':'27','MS':'28','MO':'29','MT':'30','NE':'31','NV':'32','NH':'33','NJ':'34','NM':'35','NY':'36','NC':'37','ND':'38','OH':'39','OK':'40','OR':'41','PA':'42','RI':'44','SC':'45','SD':'46','TN':'47','TX':'48','UT':'49','VT':'50','VA':'51','WA':'53','WV':'54','WI':'55','WY':'56'}
+states=['AL','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MI','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']
+electoral_votes = [9,11,6,55,9,7,3,29,16,4,4,20,11,6,6,8,8,4,10,11,16,10,6,10,3,5,6,4,14,5,29,15,3,18,7,7,20,4,9,3,11,38,6,3,13,12,5,10,3]
+electoral_counts = dict(zip(states, electoral_votes))
+
+def run_neural_net(test_state, plot_state=False, states=states_codes):
     if test_state not in states:
         print "State not available!"
         return 0
     else:
         fips_min = int(states[test_state]+'000')
         fips_max = int(str(int(states[test_state])+1)+'000')
-        print fips_min
-        print fips_max
         #Inputs: the data matrix for the counties of interest
         #Outputs: the actual fraction GOP/(GOP+DEM)
         #FIPS: The county codes corresponding to each row in Inputs/Outputs/Pop
@@ -305,24 +491,35 @@ def run_neural_net(test_state, states=states):
         training_outputs = training_outputs[np.nonzero(training_fips)]
         training_fips = training_fips[np.nonzero(training_fips)]
         training_pop = training_pop[np.nonzero(training_pop)]
-
+        test_results = np.zeros((len(test_fips), 10))
+        training_results = np.zeros((len(training_fips),10))
+        
         #Run the neural net
         print "Initiating neural network..."
-        nn = mlp(verbose=True)
-        "Training neural network..."
-        nn.fit(training_inputs, training_outputs)
-        print "Done!"
+        trials = 10
+        for i in range(trials):
+            nn = mlp(verbose=True)
+            "Training neural network..."
+            nn.fit(training_inputs, training_outputs)
+            print "Done!"
         
-        test_results = nn.predict(test_inputs)
-        bias = np.mean(test_results-test_outputs)*100.0
-        
-        print "Average deviation of " + str(np.mean(np.abs(test_results-test_outputs))*100.0)+"% +/-" + str(np.std(np.abs(test_outputs-test_results))*100)+"%"
-        if bias>0.0:
-            print "Average bias of " + str(np.abs(bias)) + "% in favor of Hillary"
-        if bias<0.0:
-            print "Average bias of " + str(np.abs(bias)) + "% in favor of Trump"
-        min_val = -0.15
-        max_val = 0.15
-        state_plot(test_fips, (test_outputs-test_results), 0, test_state, min_val, max_val)
-        return 0
-
+            tmp_results = nn.predict(test_inputs)
+            test_results[:,i] = tmp_results
+            
+            training_results[:,i] = nn.predict(training_inputs)   
+            """
+            #Interpret results     
+            bias = np.mean(test_results-test_outputs)*100.0
+            print "Average deviation of " + str(np.mean(np.abs(test_results-test_outputs))*100.0)+"% +/-" + str(np.std(np.abs(test_outputs-test_results))*100)+"%"
+            if bias>0.0:
+                print "Average bias of " + str(np.abs(bias)) + "% in favor of Hillary"
+            if bias<0.0:
+                print "Average bias of " + str(np.abs(bias)) + "% in favor of Trump"
+            """
+                
+        test_results = np.sum(test_results, axis=1)/float(trials)
+        training_results = np.sum(training_results, axis=1)/float(trials)
+        if plot_state:
+            #state_plot_data_model(test_fips, test_outputs, test_results, test_state)
+            state_plot_residual(test_fips, test_outputs, test_results, test_state, X, Y, training_results, fips_indices)
+        return (test_outputs-test_results)*100.0, test_fips
